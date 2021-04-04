5017d62567a76ceed026f995c7c8da95d49d7d9c
mephisto2120 <mephisto2120@gmail.com>
2021-04-04 16:59:27 +0200

    Scaffolding project

---
+++ src/main/java/com/tryton/small_world/server/DebtServerApplication.java
@@ -0,0 +1,12 @@
+package com.tryton.small_world.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebtServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DebtServerApplication.class, args);
+    }
+}
---
+++ src/test/java/com/tryton/small_world/server/DebtServerApplicationIntegrationTest.java
@@ -0,0 +1,12 @@
+package com.tryton.small_world.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebtServerApplicationIntegrationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
