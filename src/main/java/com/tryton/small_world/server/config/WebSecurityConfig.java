e57c449dc5ae3909ee9edbe2b983dffdf912616f
mephisto2120 <mephisto2120@gmail.com>
2021-04-24 17:36:16 +0200

    Configuring security

---
+++ src/main/java/com/tryton/small_world/server/config/WebSecurityConfig.java
@@ -0,0 +1,18 @@
+package com.tryton.small_world.server.config;
+
+import com.tryton.small_world.server.auth.JwtFilter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/test2").authenticated()
+                .antMatchers("/test3").hasRole("ADMIN")
+                .and()
+                .addFilter(new JwtFilter(authenticationManager()));
+    }
+}
