166e333cad995698b3d8bfdc8504c57fca35793e
mephisto2120 <mephisto2120@gmail.com>
2021-04-24 21:50:50 +0200

    Auth fixes

--- src/main/java/com/tryton/small_world/server/auth/JwtFilter.java
+++ src/main/java/com/tryton/small_world/server/auth/JwtFilter.java
@@ -3,6 +3,7 @@ package com.tryton.small_world.server.auth;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.impl.TextCodec;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -19,23 +20,35 @@ import java.util.Set;
 
 public class JwtFilter extends BasicAuthenticationFilter {
 
+    private static final String BEARER = "Bearer ";
+
     public JwtFilter(AuthenticationManager authenticationManager) {
         super(authenticationManager);
     }
 
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
         String header = request.getHeader("Authorization");
-        UsernamePasswordAuthenticationToken authResult = getAuthenticationByToken(header);
+        if (header == null) {
+            throw new RuntimeException("No authorization header attached!");
+        }
+        if (!header.startsWith(BEARER)) {
+            throw new RuntimeException("Wrong authorization header format!");
+        }
+
+        String token = header.replace(BEARER, "");
+        UsernamePasswordAuthenticationToken authResult = getAuthenticationByToken(token);
         SecurityContextHolder.getContext().setAuthentication(authResult);
         chain.doFilter(request, response);
     }
 
-    private UsernamePasswordAuthenticationToken getAuthenticationByToken(String header) {
-        Jws<Claims> claimsJws = Jwts.parser().setSigningKey("secretkey".getBytes())
-                .parseClaimsJws(header.replace("Bearer ", ""));
+    private UsernamePasswordAuthenticationToken getAuthenticationByToken(String token) {
+        String secretKey = TextCodec.BASE64URL.encode("topSecret12#");
+        Jws<Claims> claimsJws = Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(token);
 
-        String username = claimsJws.getBody().get("name").toString();
-        String role = claimsJws.getBody().get("role").toString();
+        String username = claimsJws.getBody().get("sub").toString();
+        String role = claimsJws.getBody().get("roles").toString();
         Set<SimpleGrantedAuthority> simpleGrantedAuthorities = Collections.singleton(new SimpleGrantedAuthority(role));
 
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, null, simpleGrantedAuthorities);
