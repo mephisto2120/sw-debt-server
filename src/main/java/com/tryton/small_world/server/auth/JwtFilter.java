a73c9ebf005ea40dd16663daa43500bf692f9e99
mephisto2120 <mephisto2120@gmail.com>
2021-09-19 13:50:20 +0200

    Auth roles fixes

--- src/main/java/com/tryton/small_world/server/auth/JwtFilter.java
+++ src/main/java/com/tryton/small_world/server/auth/JwtFilter.java
@@ -1,12 +1,7 @@
 package com.tryton.small_world.server.auth;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jws;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.impl.TextCodec;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
@@ -15,15 +10,15 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Collections;
-import java.util.Set;
 
 public class JwtFilter extends BasicAuthenticationFilter {
 
     private static final String BEARER = "Bearer ";
+    private final UsernamePasswordAuthenticationCreator authenticationCreator;
 
-    public JwtFilter(AuthenticationManager authenticationManager) {
+    public JwtFilter(AuthenticationManager authenticationManager, UsernamePasswordAuthenticationCreator authenticationCreator) {
         super(authenticationManager);
+        this.authenticationCreator = authenticationCreator;
     }
 
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
@@ -36,22 +31,27 @@ public class JwtFilter extends BasicAuthenticationFilter {
         }
 
         String token = header.replace(BEARER, "");
-        UsernamePasswordAuthenticationToken authResult = getAuthenticationByToken(token);
+        Authentication authResult = authenticationCreator.getAuthenticationByToken(token);
         SecurityContextHolder.getContext().setAuthentication(authResult);
         chain.doFilter(request, response);
     }
 
-    private UsernamePasswordAuthenticationToken getAuthenticationByToken(String token) {
-        String secretKey = TextCodec.BASE64URL.encode("topSecret12#");
-        Jws<Claims> claimsJws = Jwts.parser()
-                .setSigningKey(secretKey)
-                .parseClaimsJws(token);
-
-        String username = claimsJws.getBody().get("sub").toString();
-        String role = claimsJws.getBody().get("roles").toString();
-        Set<SimpleGrantedAuthority> simpleGrantedAuthorities = Collections.singleton(new SimpleGrantedAuthority(role));
-
-        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, null, simpleGrantedAuthorities);
-        return authenticationToken;
-    }
+//    private UsernamePasswordAuthenticationToken getAuthenticationByToken(String token) {
+//        String secretKey = TextCodec.BASE64URL.encode("topSecret12#");
+//        Jws<Claims> claimsJws = Jwts.parser()
+//                .setSigningKey(secretKey)
+//                .parseClaimsJws(token);
+//
+//        String username = claimsJws.getBody().get("sub").toString();
+//        String rolesStr = claimsJws.getBody().get("roles").toString();
+//        Set<SimpleGrantedAuthority> simpleGrantedAuthorities = getSimpleGrantedAuthorities(rolesStr);
+//        return new UsernamePasswordAuthenticationToken(username, null, simpleGrantedAuthorities);
+//    }
+//
+//    private static Set<SimpleGrantedAuthority> getSimpleGrantedAuthorities(String rolesStr) {
+//        return Arrays.stream(rolesStr.split(","))
+////                .map(roleName -> "ROLE_" + roleName)
+//                .map(SimpleGrantedAuthority::new)
+//                .collect(Collectors.toSet());
+//    }
 }
