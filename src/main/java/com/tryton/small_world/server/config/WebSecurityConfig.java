7426ea4969612e169438a491aa31eece2f38de46
mephisto2120 <mephisto2120@gmail.com>
2022-03-18 09:53:33 +0100

    Security config

--- src/main/java/com/tryton/small_world/server/config/WebSecurityConfig.java
+++ src/main/java/com/tryton/small_world/server/config/WebSecurityConfig.java
@@ -12,6 +12,21 @@ import static java.util.Objects.requireNonNull;
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private static final String[] AUTH_WHITELIST = {
+            // -- Swagger UI v2
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**",
+            // -- Swagger UI v3 (OpenAPI)
+            "/v3/api-docs/**",
+            "/swagger-ui/**"
+            // other public endpoints of your API may be appended to this array
+    };
+
     private final UsernamePasswordAuthenticationCreator authenticationCreator;
 
     public WebSecurityConfig(UsernamePasswordAuthenticationCreator authenticationCreator) {
@@ -24,7 +39,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/test2").authenticated()
                 .antMatchers("/test3").hasAuthority("ADMIN")
 //                .antMatchers("/test3").hasRole("ADMIN")
-                .antMatchers("**/swagger-ui.html#").permitAll()
+                .antMatchers(AUTH_WHITELIST).permitAll()
                 .and()
                 .addFilter(jwtFilter());
     }
